dd71d5d39bd4a3f4fb487d6a8c9cc535f545bef2
Leah Jones <leah@l12s.com>
2015-06-18 17:13:04 -0700

    Drastically simplifies the site content, to serve a suuuuper basic site aggregating social media presence

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -6,8 +6,9 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var shell = require('gulp-shell');
 
-gulp.task('buildJekyll', shell.task('jekyll build --config _config.yml,_config.dev.yml'));
+gulp.task('buildJekyll:prod', shell.task('jekyll build --config _config.yml'));
+gulp.task('buildJekyll:dev', shell.task('jekyll build --config _config.yml,_config.dev.yml'));
 
 gulp.task('build', function(cb) {
-  runSequence('clean', 'buildJekyll', ['browserify', 'sass'], cb);
+  runSequence('clean', 'buildJekyll:dev', 'sass', cb);
 });
